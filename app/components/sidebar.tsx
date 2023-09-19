4a9008d2d390e8e8650f95d9901f479471e1c904
xiaoheiwa <47276687+xiaoheiwa@users.noreply.github.com>
2023-09-19 20:38:04 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,10 +145,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+         小白不懂科技
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          www.xbbdkj.com
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
