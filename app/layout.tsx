d407daed3c49d64e773ab52b7d4b2be78061c5e7
xiaoheiwa <47276687+xiaoheiwa@users.noreply.github.com>
2024-01-28 19:53:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "小白不懂科技-智能助手",
+  title: "小黑丸-智能助手",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "小黑丸- 智能助手",
     statusBarStyle: "default",
   },
 };
